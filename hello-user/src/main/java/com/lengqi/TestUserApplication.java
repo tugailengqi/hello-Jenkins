f95f3920e8b5aa250232fd2aaa20152fceceb327
lengqi <1982182894@qq.com>
2021-05-29 17:31:18 +0800

    另一个模块

---
+++ hello-user/src/main/java/com/lengqi/TestUserApplication.java
@@ -0,0 +1,15 @@
+package com.lengqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lengqi
+ * @DATE: 2021/5/29
+ */
+@SpringBootApplication
+public class TestUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestUserApplication.class,args);
+    }
+}
--- hello-user/src/main/java/com/lengqi/controller/UserController.java
+++ hello-user/src/main/java/com/lengqi/controller/UserController.java
@@ -1,19 +1,23 @@
 package com.lengqi.controller;
 
+import com.lengqi.common.constant.Constant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author lengqi
- * @DATE: 2021/5/28
+ * @DATE: 2021/5/29
  */
 @RestController
-@RequestMapping("/test")
-public class TestController {
-
-    @GetMapping("/hello")
-    public String hello(){
-        return Constant.HELLOS;
+@RequestMapping("/user")
+public class UserController {
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/**")
+    public String getUser(){
+        return Constant.SA_TOKEN;
     }
 }
